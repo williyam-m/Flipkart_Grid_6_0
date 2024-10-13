6d472903478295f081dba9b1266bd80fdafc1324
williyam-m <williyam64@gmail.com>
2024-10-13 18:22:02 +0530

    create FreshnessPrediction database table model

--- freshness_detector/models.py
+++ freshness_detector/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class FreshnessPrediction(models.Model):
+    image = models.ImageField(upload_to='uploads/')
+    predicted_class = models.CharField(max_length=100)
+    confidence_score = models.FloatField()
+    time_taken = models.FloatField()
+    uploaded_at = models.DateTimeField(auto_now_add=True)
