029d644565233d8128d7bb13a84042086643a4d2
williyam-m <williyam64@gmail.com>
2024-10-16 17:57:21 +0530

    feat : implement extract_text_from_image logic using pytesseract

--- OCR/views.py
+++ OCR/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+import pytesseract
 
-# Create your views here.
+def extract_text_from_image(preprocessed_image):
+
+    text = pytesseract.image_to_string(preprocessed_image)
+    return text
