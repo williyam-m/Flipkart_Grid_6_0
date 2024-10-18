8eb220796f326497afdc69c1fdef32eb64be5906
williyam-m <williyam64@gmail.com>
2024-10-18 18:38:34 +0530

    refactor css static files

--- Flipkart_Grid_6_0/settings.py
+++ Flipkart_Grid_6_0/settings.py
@@ -127,6 +127,7 @@ STATIC_URL = 'static/'
 
 
 STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'user', 'static'),
     os.path.join(BASE_DIR, 'dashboard', 'static'),
     os.path.join(BASE_DIR, 'freshness_detector', 'static'),
